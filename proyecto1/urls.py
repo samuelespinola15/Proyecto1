c6feab1cf13bcabc6f7ae8c73446b0e4d2efcc50
Samuel <samuelespinola15@gmail.com>
2021-12-08 23:44:19 -0300

    segundo

--- proyecto1/urls.py
+++ proyecto1/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from proyecto1.views import saludo,dameFecha,calculaEdad
-
+#probando git
 
 urlpatterns = [
     path('admin/', admin.site.urls),
